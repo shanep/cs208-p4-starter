82ed9a7cf4159ff50c0e2b6abd79de70d1167e8a
Shane K. Panter <shanepanter@boisestate.edu>
2023-10-26 14:01:39 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,6 +6,12 @@ const sqlite3 = sql.verbose()
 // Create an in memory table to use
 const db = new sqlite3.Database(':memory:')
 
+// This is just for testing you would not want to create the table every
+// time you start up the app feel free to improve this code :)
+db.run(`CREATE TABLE todo (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    task TEXT NOT NULL)`)
+
 const app = express()
 app.use(express.static('public'))
 app.set('views', 'views')
@@ -13,9 +19,21 @@ app.set('view engine', 'pug')
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', function (req, res) {
-  //TODO You will need to load your tasks from the data base here
+  //TODO You will need to do a SQL select here
   console.log('GET called')
   res.render('index')
+})
+
+app.post('/', function (req, res) {
+  console.log('adding todo item')
+  //TODO You will need to to do a SQL Insert here
+  res.redirect('/')
+})
+
+app.post('/delete', function (req, res) {
+  console.log('deleting todo item')
+  //TODO you will need to delete here
+  res.redirect('/')
 })
 
 // Start the web server
